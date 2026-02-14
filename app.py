9836ae7cdc3971e69be2521bb1b3be8e978877d8
KgotsoMabuse04 <kgotso.mabuse15@gmail.com>
2026-02-14 18:00:31 +0200

    Commented out background thread to fix deployment

--- app.py
+++ app.py
@@ -130,9 +130,9 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
         
-    thread = threading.Thread(target=reminder_checker)
-    thread.daemon = True
-    thread.start()
+   # thread = threading.Thread(target=reminder_checker)
+   # thread.daemon = True
+   # thread.start()
     
     
     app.run(host="0.0.0.0", port=5000, debug=True) 
